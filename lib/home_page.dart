0e70a4961f883c8c557e569e03787ac0d51d50a0
BigB97 <bigbofficial01@gmail.com>
2021-04-25 15:41:29 +0100

    update

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import 'iconWig.dart';
 import 'newcards.dart';
 
-Color activecolor= Color(0xFF1D1E33);
-Color inactivecolor= Color(0xFF111328);
+Color activecolor = Color(0xFF1D1E33);
+Color inactivecolor = Color(0xFF111328);
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
+
 class _HomePageState extends State<HomePage> {
   Color maleColor = inactivecolor;
   Color femaleColor = inactivecolor;
-  void toggly(int gender){
-    if(gender == 1 ){
-      if(maleColor == inactivecolor){
+
+  void toggly(int gender) {
+    if (gender == 1) {
+      if (maleColor == inactivecolor) {
         maleColor = activecolor;
         femaleColor = inactivecolor;
       } else {
         maleColor = inactivecolor;
       }
     }
-    if(gender == 2){
-      if(femaleColor == inactivecolor){
+    if (gender == 2) {
+      if (femaleColor == inactivecolor) {
         femaleColor = activecolor;
         maleColor = inactivecolor;
       } else {
@@ -32,6 +35,7 @@ class _HomePageState extends State<HomePage> {
       }
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -45,45 +49,44 @@ class _HomePageState extends State<HomePage> {
                 child: Row(
               children: [
                 Expanded(
-                  child: GestureDetector(
-                    onTap: (){
+                  child: newCard(
+                    onPress: () {
                       setState(() {
                         toggly(1);
                       });
                     },
-                    child: newCard(
-                      maleColor,
-                      iconWig(FontAwesomeIcons.mars, 'Male'),
-                    ),
+                    colour: maleColor,
+                    cardItem: iconWig(FontAwesomeIcons.mars, 'Male'),
                   ),
                 ),
                 Expanded(
-                  child: GestureDetector(
-                    onTap: (){
+                  child: newCard(
+                    onPress: () {
                       setState(() {
                         toggly(2);
                       });
                     },
-                    child: newCard(
-                      femaleColor,
-                      iconWig(FontAwesomeIcons.venus, 'Female'),
-                    ),
+                    colour: femaleColor,
+                    cardItem: iconWig(FontAwesomeIcons.venus, 'Female'),
                   ),
                 ),
               ],
             )),
             Expanded(
                 child: newCard(
-                    inactivecolor,
-                    Column(
-                      children: [
-                      ],
-                    ))),
+              colour: inactivecolor,
+            )),
             Expanded(
                 child: Row(
               children: [
-                Expanded(child: newCard(inactivecolor, Column())),
-                Expanded(child: newCard(inactivecolor, Column())),
+                Expanded(
+                    child: newCard(
+                  colour: inactivecolor,
+                )),
+                Expanded(
+                    child: newCard(
+                  colour: inactivecolor,
+                )),
               ],
             )),
             Container(
@@ -97,12 +100,10 @@ class _HomePageState extends State<HomePage> {
                   style: TextStyle(fontSize: 20),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
     );
   }
-
-
 }
--- lib/newcards.dart
+++ lib/newcards.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 // ignore: camel_case_types
 class newCard extends StatelessWidget {
-  newCard(@required this.colour,this.cardItem);
+  newCard({this.colour, this.cardItem, this.onPress});
   final Widget cardItem;
   final Color colour;
+  final Function onPress;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardItem,
-      width: double.infinity,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: cardItem,
+        width: double.infinity,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
       ),
     );
   }
